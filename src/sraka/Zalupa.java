fef20011e9bedb89afd3d1105c0c336910563e03
Ostap <ostapben84@gmail.com>
2022-01-31 21:04:20 +0200

    it's a joke

---
+++ src/sraka/Zalupa.java
@@ -0,0 +1,10 @@
+package sraka;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class Zalupa {
+    public static void main(String[] args) {
+        System.out.println("Guk");
+    }
+}
