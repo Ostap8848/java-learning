6e2980ccf2526a1fe6fc39b392bc836559c03046
Ostap <ostapben84@gmail.com>
2022-01-31 20:29:32 +0200

    first git example

---
+++ src/variables/Example1.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int integer = 5;
+        String str = "blank";
+        boolean b = true;
+        System.out.println(integer + "\n" +  str + "\n" + b);
+    }
+}
