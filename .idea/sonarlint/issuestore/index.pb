59636325cac92000ce7dfd51e35d4fe3233e6e4a
Ostap <ostapben84@gmail.com>
2022-02-01 10:04:58 +0200

    this is second example

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+P
+ src/introduction/HelloWorld.java,7\b\7bef3cb721c9126e5c1d3c597eec45f02b30cc72
+K
+src/variables/Example1.java,e\4\e4de5bfb9cbf037cc00b64e0fd8811d8a0d8fde4
---
+++ src/variables/Example2.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int a = 100, b = 200, c = 300;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a + " " + b + " " + c);
+    }
+}
